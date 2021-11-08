34f9bf2ad4e67b4f871c6b85a67844e8f4387570
Alex Standiford <a@alexstandiford.com>
2021-11-08 06:54:37 -0600

    Initial commit.

--- index.php
+++ index.php
@@ -7,8 +7,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 // Load Underpin, and its dependencies.
 if(file_exists( plugin_dir_path( __FILE__ ) . 'vendor/autoload.php') ) {
 	$autoload = plugin_dir_path( __FILE__ ) . 'vendor/autoload.php';
+	require_once( $autoload );
 } else{
 	wp_die('Could not load vendor dependencies. Are you sure you ran composer install?');
 }
-
-require_once( $autoload );
